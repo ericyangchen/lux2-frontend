5e12c462303c4cb0772c565979797e93ea7e61e3
ericyangchen <yericchenyang@gmail.com>
2024-11-27 21:38:46 +0800

    fix: valid page

--- src/pages/admin/transaction-handling/index.tsx
+++ src/pages/admin/transaction-handling/index.tsx
@@ -1,11 +1,10 @@
-// import { ApplicationHeader } from "@/modules/common/ApplicationHeader";
-// import { TransactionHandlingView } from "@/modules/admin/transaction-handling/TransactionHandlingView";
+import { ApplicationHeader } from "@/modules/common/ApplicationHeader";
 
-// export default function AdminTransactionListPage() {
-//   return (
-//     <div className="flex flex-col h-full">
-//       <ApplicationHeader title="訂單處理" />
-//       <TransactionHandlingView />
-//     </div>
-//   );
-// }
+export default function AdminTransactionListPage() {
+  return (
+    <div className="flex flex-col h-full">
+      <ApplicationHeader title="訂單處理" />
+      {/* <TransactionHandlingView /> */}
+    </div>
+  );
+}
