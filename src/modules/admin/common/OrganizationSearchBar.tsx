58b1508e3a850dab4b5bbdaab59d6f627989671d
ericyangchen <yericchenyang@gmail.com>
2025-07-09 19:14:11 +0800

    feat: search bar ui

--- src/modules/admin/common/OrganizationSearchBar.tsx
+++ src/modules/admin/common/OrganizationSearchBar.tsx
@@ -88,7 +88,7 @@ export function OrganizationSearchBar({
           />
         </ComboboxButton>
 
-        <ComboboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base sm:text-sm border">
+        <ComboboxOptions className="absolute z-10 mt-1 max-h-[480px] w-full overflow-auto rounded-md bg-white py-1 text-base sm:text-sm border">
           {query === "" && (
             <ComboboxOption
               className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-purple-700 data-[focus]:text-white"
